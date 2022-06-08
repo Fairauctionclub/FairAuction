633c6446d4314535715186cebd75a35feb124259
teamfairauction <94896055+teamfairauction@users.noreply.github.com>
2022-06-08 14:59:25 +0800

    a

---
+++ contracts/EasyAuction.sol
@@ -0,0 +1,131 @@
+pragma solidity >=0.6.8;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./libraries/IterableOrderedOrderSet.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./libraries/IdToAddressBiMap.sol";
+import "./libraries/SafeCast.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/AllowListVerifier.sol";
+
+contract EasyAuction is Ownable {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint64;
+    using SafeMath for uint96;
+    using SafeMath for uint256;
+    using SafeCast for uint256;
+    using IterableOrderedOrderSet for IterableOrderedOrderSet.Data;
+    using IterableOrderedOrderSet for bytes32;
+    using IdToAddressBiMap for IdToAddressBiMap.Data;
+
+    modifier atStageOrderPlacement(uint256 auctionId) {
+        require(
+            block.timestamp < auctionData[auctionId].auctionEndDate,
+            "no longer in order placement phase"
+        );
+        _;
+    }
+
+    modifier atStageOrderPlacementAndCancelation(uint256 auctionId) {
+        require(
+            block.timestamp < auctionData[auctionId].orderCancellationEndDate,
+            "no longer in order placement and cancelation phase"
+        );
+        _;
+    }
+
+    modifier atStageSolutionSubmission(uint256 auctionId) {
+        {
+            uint256 auctionEndDate = auctionData[auctionId].auctionEndDate;
+            require(
+                auctionEndDate != 0 &&
+                    block.timestamp >= auctionEndDate &&
+                    auctionData[auctionId].clearingPriceOrder == bytes32(0),
+                "Auction not in solution submission phase"
+            );
+        }
+        _;
+    }
+
+    modifier atStageFinished(uint256 auctionId) {
+        require(
+            auctionData[auctionId].clearingPriceOrder != bytes32(0),
+            "Auction not yet finished"
+        );
+        _;
+    }
+
+    event NewSellOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event CancellationSellOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event ClaimedFromOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event NewUser(uint64 indexed userId, address indexed userAddress);
+    event NewAuction(
+        uint256 indexed auctionId,
+        IERC20 indexed _auctioningToken,
+        IERC20 indexed _biddingToken,
+        uint256 orderCancellationEndDate,
+        uint256 auctionEndDate,
+        uint64 userId,
+        uint96 _auctionedSellAmount,
+        uint96 _minBuyAmount,
+        uint256 minimumBiddingAmountPerOrder,
+        uint256 minFundingThreshold,
+        address allowListContract,
+        bytes allowListData
+    );
+    event AuctionCleared(
+        uint256 indexed auctionId,
+        uint96 soldAuctioningTokens,
+        uint96 soldBiddingTokens,
+        bytes32 clearingPriceOrder
+    );
+    event UserRegistration(address indexed user, uint64 userId);
+
+    struct AuctionData {
+        IERC20 auctioningToken;
+        IERC20 biddingToken;
+        uint256 orderCancellationEndDate;
+        uint256 auctionEndDate;
+        bytes32 initialAuctionOrder;
+        uint256 minimumBiddingAmountPerOrder;
+        uint256 interimSumBidAmount;
+        bytes32 interimOrder;
+        bytes32 clearingPriceOrder;
+        uint96 volumeClearingPriceOrder;
+        bool minFundingThresholdNotReached;
+        bool isAtomicClosureAllowed;
+        uint256 feeNumerator;
+        uint256 minFundingThreshold;
+    }
+    mapping(uint256 => IterableOrderedOrderSet.Data) internal sellOrders;
+    mapping(uint256 => AuctionData) public auctionData;
+    mapping(uint256 => address) public auctionAccessManager;
+    mapping(uint256 => bytes) public auctionAccessData;
+
+    IdToAddressBiMap.Data private registeredUsers;
+    uint64 public numUsers;
+    uint256 public auctionCounter;
+
+    constructor() public Ownable() {}
+
+    uint256 public feeNumerator = 0;
+    uint256 public constant FEE_DENOMINATOR = 1000;
+    uint64 public feeReceiverUserId = 1;
+
+}
