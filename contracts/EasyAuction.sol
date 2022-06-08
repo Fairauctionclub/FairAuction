13405034f074fddf3ef803a699429310a77addc1
teamfairauction <94896055+teamfairauction@users.noreply.github.com>
2022-06-08 15:01:04 +0800

    Update EasyAuction.sol

--- contracts/EasyAuction.sol
+++ contracts/EasyAuction.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./libraries/IterableOrderedOrderSet.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "./libraries/IdToAddressBiMap.sol";
+//import "./libraries/IdToAddressBiMap.sol";
 import "./libraries/SafeCast.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/AllowListVerifier.sol";
