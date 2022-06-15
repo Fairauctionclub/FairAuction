6decb8c3485dfba6fa3050401828c90f53e4f550
Fairauction <94896055+Fairauctionclub@users.noreply.github.com>
2022-06-15 17:26:31 +0800

    Update IController.sol

--- contracts/interfaces/IController.sol
+++ contracts/interfaces/IController.sol
@@ -6,4 +6,9 @@ interface IController {
     function getBurnFeeRate(address filterAdminAddr) external view returns (uint256);
 
     function isEmergencyWithdrawOpen(address filter) external view returns (bool);
+    
+    function mintFragTo(address to, uint256 rawAmount) external returns (uint256 received);
+
+    function burnFragFrom(address from, uint256 rawAmount) external returns (uint256 paid);
+
 }
