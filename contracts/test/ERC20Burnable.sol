7fa84157a12bb7a08b6147cba6a3de2c5d9160d6
Fairauction <94896055+Fairauctionclub@users.noreply.github.com>
2022-06-29 14:49:44 +0800

    test

--- contracts/test/ERC20Burnable.sol
+++ contracts/test/ERC20Burnable.sol
@@ -3,10 +3,7 @@ pragma solidity >=0.6.8;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Burnable is ERC20 {
-    constructor(string memory name, string memory symbol)
-        public
-        ERC20(symbol, name)
-    {}
+    constructor(string memory name, string memory symbol) public ERC20(symbol, name) {}
 
     function burn(address account, uint256 amount) public {
         _burn(account, amount);
